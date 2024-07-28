3d1f23249ba92823a925959109385c4286a718c3
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-07-28 18:25:24 -0400

    create medical record nft

--- blockchain/app/contracts/MedicalRecord.sol
+++ blockchain/app/contracts/MedicalRecord.sol
@@ -5,82 +5,20 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import {NftOwner} from "./access control/NftOwner.sol";
+import {GenericNft} from "./GenericNft.sol";
 
 
-contract MedicalRecord is ERC721, ERC721Enumerable, ERC721URIStorage, NftOwner {
-    uint256 private _nextTokenId;
-    address internal _doctorNftContractAddress;
-
+contract MedicalRecord is GenericNft{
+ 
     constructor(
-        address doctorNftContract
-    ) ERC721("MedicalRecord", "MRTK") NftOwner(doctorNftContract){
-        _doctorNftContractAddress = doctorNftContract;
-    }
+        address doctorNftContractAddress
+    ) GenericNft("MedicalRecord", "MRTK", doctorNftContractAddress){}
     //TODO - fix this
     function _baseURI() internal pure override returns (string memory) {
         return "kokokokkko";
     }
 
-    function safeMint(address to, string memory uri) public onlyNftOwner() {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-    }
-
-    function transferFrom(address from, address to, uint256 tokenId) public virtual override(ERC721, IERC721) {
-        require(false, "Transfers are currently locked");
-    }
-
-    function setTokenURI(uint256 tokenId, string memory _newUri) public onlyNftOwner() {
+    function setTokenURI(uint256 tokenId, string memory _newUri) public onlyNftOwner {
         _setTokenURI(tokenId, _newUri);
-    }
-
-    // //TODO - maybe migrate this to a doctor access control contract or something
-    // modifier onlyDoctor(address doctorNftContractAddress) {
-    //     require(_checkNFTOwnership(doctorNftContractAddress, _msgSender()), "Not a doctor");
-    //     _;
-    // }
-
-    // function _checkNFTOwnership(
-    //     address nftContractAddress, 
-    //     address potentialOwner
-    // ) internal view returns (bool) {
-    //     return IERC721(nftContractAddress).balanceOf(potentialOwner) > 0;
-    // }
-
-    // The following functions are overrides required by Solidity.
-
-    function _update(address to, uint256 tokenId, address auth)
-        internal
-        override(ERC721, ERC721Enumerable)
-        returns (address)
-    {
-        return super._update(to, tokenId, auth);
-    }
-
-    function _increaseBalance(address account, uint128 value)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._increaseBalance(account, value);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable, ERC721URIStorage)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
     }
 }
