951c3fa307875620830673465b51612a3044544d
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-07-30 19:53:04 -0400

    create admin nft

--- blockchain/app/contracts/Admin.sol
+++ blockchain/app/contracts/Admin.sol
@@ -4,17 +4,56 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import {GenericNft} from "./GenericNft.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-
-contract Admin is GenericNft {
+contract Admin is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
+    uint256 private _nextTokenId;
 
     constructor(
-        address doctorNftContractAddress
-    ) GenericNft("Patient", "PTK", doctorNftContractAddress) {}
+        address initialOwner
+    ) ERC721("Admin", "ATK") Ownable(initialOwner) {}
 
     function _baseURI() internal pure override returns (string memory) {
-        return "koko";
+        return "inkjnjn";
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        address owner = Ownable.owner();
+        approve(owner, tokenId);
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override(ERC721, IERC721) {
+        //TODO - add some type of custom error
+        require(false, "Transfers are currently locked");
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
