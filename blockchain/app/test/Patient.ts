6f2745a455cd8ecbcecfaa9d74b446b39daddb56
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-08-07 19:43:12 -0400

    create fixture to deploy admin nft

--- blockchain/app/test/Patient.ts
+++ blockchain/app/test/Patient.ts
@@ -5,23 +5,22 @@ import {
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre from "hardhat";
+import { token } from "../typechain-types/@openzeppelin/contracts";
+import { Patient } from "../typechain-types";
 
 describe("Patient", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
-
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
+  async function deployAdminFixture() {
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await hre.ethers.getSigners();
+    const [owner, account1, account2] = await hre.ethers.getSigners();
 
-    const Lock = await hre.ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const AdminFactory = await hre.ethers.getContractFactory("Admin");
+    const adminDeployed = await AdminFactory.deploy(await owner.getAddress());
+    const admin = await adminDeployed.waitForDeployment();
+    return { admin, owner, account1, account2 };
+  }
 
     return { lock, unlockTime, lockedAmount, owner, otherAccount };
   }
