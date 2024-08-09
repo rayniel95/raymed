239937548b65e2cf4334d3f0a0fa85adc60d5861
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-08-09 15:50:41 -0400

    create patient base test

--- blockchain/app/test/Patient.ts
+++ blockchain/app/test/Patient.ts
@@ -1,132 +1,134 @@
 import {
-  time,
-  loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-import { expect } from "chai";
-import hre from "hardhat";
-import { token } from "../typechain-types/@openzeppelin/contracts";
-import { Patient } from "../typechain-types";
+    time,
+    loadFixture,
+  } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+  import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+  import { expect } from "chai";
+  import hre from "hardhat";
+  import { token } from "../typechain-types/@openzeppelin/contracts";
+  import { Doctor, Patient } from "../typechain-types";
+  
+  describe("Patient", function () {
+    // We define a fixture to reuse the same setup in every test.
+    // We use loadFixture to run this setup once, snapshot that state,
+    // and reset Hardhat Network to that snapshot in every test.
+    async function deployAdminFixture() {
+      // Contracts are deployed using the first signer/account by default
+      const [deployer, superUser, adminUser, doctorUser] = await hre.ethers.getSigners();
+  
+      const AdminFactory = await hre.ethers.getContractFactory("Admin");
+      const adminDeployed = await AdminFactory.connect(deployer).deploy(await superUser.getAddress());
+      const admin = await adminDeployed.waitForDeployment();
+      return { admin, deployer, superUser, adminUser, doctorUser };
+    }
+  
+    async function mintFirstAdmin() {
+      // Contracts are deployed using the first signer/account by default
+      const { admin, deployer, superUser, adminUser, doctorUser } = await loadFixture(deployAdminFixture);
+  
+      await admin.connect(superUser).safeMint(adminUser, 'http://test.url');
+      return { admin, deployer, superUser, adminUser, doctorUser };
+    }
+  
+    async function deployDoctorFixture() {
+      const { admin, deployer, superUser, adminUser, doctorUser } = await loadFixture(mintFirstAdmin);
+  
+      const DoctorFactory = await hre.ethers.getContractFactory("Doctor");
+      const doctorDeployed = await DoctorFactory.connect(deployer).deploy(await admin.getAddress());
+      const doctor = await doctorDeployed.waitForDeployment();
+      return { doctor, admin, deployer, superUser, adminUser, doctorUser };
+    }
+  
+    async function mintFirstDoctor() {
+        // Contracts are deployed using the first signer/account by default
+        const {doctor, admin, deployer, superUser, adminUser, doctorUser} = await loadFixture(deployDoctorFixture);
+  
+        await doctor.connect(adminUser).safeMint(doctorUser, 'http://test.url');
+        return { doctor, admin, deployer, superUser, adminUser, doctorUser };
+    }
+  
+    async function deployPatientFixture() {
+        const { doctor, admin, deployer, superUser, adminUser, doctorUser } = await loadFixture(mintFirstDoctor);
+    
+        const PatientFactory = await hre.ethers.getContractFactory("Patient");
+        const patientDeployed = await PatientFactory.connect(deployer).deploy(await doctor.getAddress());
+        const patient = await patientDeployed.waitForDeployment();
+        return { patient, doctor, admin, deployer, superUser, adminUser, doctorUser };
+      }
+    
+      async function mintFirstPatient() {
+          // Contracts are deployed using the first signer/account by default
+          const {doctor, admin, deployer, superUser, adminUser, doctorUser} = await loadFixture(deployPatientFixture);
+    
+          await doctor.connect(adminUser).safeMint(doctorUser, 'http://test.url');
+          return { doctor, admin, deployer, superUser, adminUser, doctorUser };
+      }
 
-describe("Patient", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployAdminFixture() {
-    // Contracts are deployed using the first signer/account by default
-    const [superUser, account1, account2] = await hre.ethers.getSigners();
-
-    const AdminFactory = await hre.ethers.getContractFactory("Admin");
-    const adminDeployed = await AdminFactory.deploy(await superUser.getAddress());
-    const admin = await adminDeployed.waitForDeployment();
-    return { admin, superUser, account1, account2 };
-  }
-
-  async function mintFirstAdmin() {
-    // Contracts are deployed using the first signer/account by default
-    const { admin, owner, account1, account2 } = await loadFixture(deployAdminFixture);
-
-    await admin.safeMint(account1)
-    const tokenId = 0;
-    return { admin, owner,  account1, account2 };
-  }
-
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
+    describe("Deployment", function () {
+      it("Should say owner contract address is admin contract address", async function () {
+        const { doctor, admin, deployer, superUser, adminUser, doctorUser } = await loadFixture(deployDoctorFixture);
+  
+        expect(await doctor.nft()).to.equal(await admin.getAddress());
+      });
+  
+      it("Should have symbol DTK", async function () {
+        const { doctor, admin, deployer, superUser, adminUser, doctorUser } = await loadFixture(deployDoctorFixture);
+  
+        expect(await doctor.symbol()).to.equal("DTK");
+      });
     });
-
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await hre.ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await hre.ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
+  
+    describe("Basic opertions", function () {
+      describe("Mint related tests", function () {
+        it("Should mint a nft", async function () {
+          const { doctor, admin, deployer, superUser, adminUser, doctorUser } = await loadFixture(deployDoctorFixture);
+  
+          expect(await doctor.connect(adminUser).safeMint(doctorUser, 'http://test.url')).to.not.be.reverted;
+          expect(await doctor.balanceOf(doctorUser)).to.equal(1);
+        });
+  
+        it("Should not allow to mint a nft", async function () {
+          const { doctor, admin, deployer, superUser, adminUser, doctorUser } = await loadFixture(deployDoctorFixture);
+  
+          await expect(
+            (doctor.connect(superUser) as Doctor).safeMint(doctorUser, 'https://test.url')
+          ).to.be.revertedWithCustomError(
+            doctor,
+            "NftOwnerUnauthorizedAccount"
+          ).withArgs(superUser);
+        });
+      })
+  
+      describe("Transfers related test", function() {
+        it("Should not to allow transfer the nft", async function () {
+          const { doctor, admin, deployer, superUser, adminUser, doctorUser  } = await loadFixture(mintFirstDoctor);
+          
+          await expect(
+            (doctor.connect(doctorUser) as Doctor).transferFrom(doctorUser, adminUser, 0)
+          ).to.be.revertedWith("Transfers are currently locked");
+          await expect(
+            doctor.transferFrom(doctorUser, adminUser, 0)
+          ).to.be.revertedWith("Transfers are currently locked");
+        });
+      })
+  
+      describe("Burn related test", function() {
+        it("Should not to allow burn the nft", async function () {
+          const { doctor, admin, deployer, superUser, adminUser, doctorUser  } = await loadFixture(mintFirstDoctor);
+          
+          await expect(
+            (doctor.connect(doctorUser) as Doctor).burn(0)
+          ).to.be.revertedWithCustomError(doctor, "NftOwnerUnauthorizedAccount");
+    
+        });
+        it("Should allow burn the nft", async function () {
+          const { doctor, admin, deployer, superUser, adminUser, doctorUser  } = await loadFixture(mintFirstDoctor);
+          
+          expect(
+            await (doctor.connect(adminUser) as Doctor).burn(0)
+          ).to.not.reverted;
+        });
+      })
+    })
   });
-
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
-
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
-  });
-});
+  
