7710ddb1d05f93ecdaef7ef0729e47780aa67bd0
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-08-15 19:46:05 -0400

    add more specific fields

--- frontend/ui/src/app/components/resources/list/AdminList.tsx
+++ frontend/ui/src/app/components/resources/list/AdminList.tsx
@@ -4,8 +4,9 @@ export const AdminList = () => (
     <List>
         <Datagrid>
             <TextField source="id" />
-            <TextField source="name" />
-            <TextField source="lastName" />
+            <TextField source="firstName" />
+            <TextField source="secondName" />
+            <TextField source="firstLastName" />
             <TextField source="secondLastName" />
             <EmailField source="email" />
             <TextField source="phone" />
