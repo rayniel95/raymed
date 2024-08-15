9e209eba95f95dc15c4b8c71668f6cbaa44d5986
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-08-15 19:47:04 -0400

    add base resources for app actors

--- frontend/ui/src/app/components/AdminApp.tsx
+++ frontend/ui/src/app/components/AdminApp.tsx
@@ -1,24 +1,26 @@
 "use client"; // remove this line if you choose Pages Router
 import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import { AdminList } from "./resources/list/AdminList";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const AdminApp = () => (
   <Admin dataProvider={dataProvider}>
     <Resource
-      name="users"
-      list={ListGuesser}
+      name="admins"
+      list={AdminList}
       edit={EditGuesser}
       recordRepresentation="name"
     />
     <Resource
-      name="posts"
+      name="doctors"
       list={ListGuesser}
       edit={EditGuesser}
       recordRepresentation="title"
     />
-    <Resource name="comments" list={ListGuesser} edit={EditGuesser} />
+    <Resource name="patients" list={ListGuesser} edit={EditGuesser} />
+    <Resource name="medicalRecords" list={ListGuesser} edit={EditGuesser} />
   </Admin>
 );
 
