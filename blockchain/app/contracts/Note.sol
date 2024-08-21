c751741c292fc7aea4d2b2c763ee262e554a768c
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-08-20 20:02:13 -0400

    split medical record model

---
+++ blockchain/app/contracts/Note.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {GenericNft} from "./common/GenericNft.sol";
+
+
+contract Note is GenericNft{
+ 
+    constructor(
+        address doctorNftContractAddress
+    ) GenericNft("Note", "MRTK", doctorNftContractAddress){}
+    //TODO - fix this
+    function _baseURI() internal pure override returns (string memory) {
+        return "http://medical.record";
+    }
+
+    function setTokenURI(uint256 tokenId, string memory _newUri) public onlyNftOwner {
+        _setTokenURI(tokenId, _newUri);
+    }
+
+    function burn(uint256 tokenId) public override {
+        //TODO - add some type of custom error
+        require(false, "burn is loked");
+    }
+}
