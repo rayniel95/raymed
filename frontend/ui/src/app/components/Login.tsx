9c1ae93b08410f2e069388fbd655bd4daea61394
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-08-28 20:42:27 -0400

    add base login component

--- frontend/ui/src/app/components/Login.tsx
+++ frontend/ui/src/app/components/Login.tsx
@@ -3,8 +3,6 @@
 
 "use client";
 
-// import "./globals.css";
-
 import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base";
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
 import { Web3Auth } from "@web3auth/modal";
@@ -38,175 +36,165 @@ const web3auth = new Web3Auth({
   privateKeyProvider,
 });
 
-// function App() {
-//   const [provider, setProvider] = useState<IProvider | null>(null);
-//   const [loggedIn, setLoggedIn] = useState(false);
+export default function Login() {
+  const [provider, setProvider] = useState<IProvider | null>(null);
+  const [loggedIn, setLoggedIn] = useState(false);
 
-//   useEffect(() => {
-//     const init = async () => {
-//       try {
-//         await web3auth.initModal();
-//         setProvider(web3auth.provider);
+  useEffect(() => {
+    const init = async () => {
+      try {
+        await web3auth.initModal();
+        setProvider(web3auth.provider);
 
-//         if (web3auth.connected) {
-//           setLoggedIn(true);
-//         }
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
+        if (web3auth.connected) {
+          setLoggedIn(true);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-//     init();
-//   }, []);
+    init();
+  }, []);
 
-//   const login = async () => {
-//     const web3authProvider = await web3auth.connect();
-//     setProvider(web3authProvider);
-//     if (web3auth.connected) {
-//       setLoggedIn(true);
-//     }
-//   };
+  const login = async () => {
+    const web3authProvider = await web3auth.connect();
+    setProvider(web3authProvider);
+    if (web3auth.connected) {
+      setLoggedIn(true);
+    }
+  };
 
-//   const getUserInfo = async () => {
-//     const user = await web3auth.getUserInfo();
-//     uiConsole(user);
-//   };
+  // const getUserInfo = async () => {
+  //   const user = await web3auth.getUserInfo();
+  //   uiConsole(user);
+  // };
 
-//   const logout = async () => {
-//     await web3auth.logout();
-//     setProvider(null);
-//     setLoggedIn(false);
-//     uiConsole("logged out");
-//   };
+  // const logout = async () => {
+  //   await web3auth.logout();
+  //   setProvider(null);
+  //   setLoggedIn(false);
+  //   uiConsole("logged out");
+  // };
 
-//   // Check the RPC file for the implementation
-//   const getAccounts = async () => {
-//     if (!provider) {
-//       uiConsole("provider not initialized yet");
-//       return;
-//     }
-//     const address = await RPC.getAccounts(provider);
-//     uiConsole(address);
-//   };
+  // // Check the RPC file for the implementation
+  // const getAccounts = async () => {
+  //   if (!provider) {
+  //     uiConsole("provider not initialized yet");
+  //     return;
+  //   }
+  //   const address = await RPC.getAccounts(provider);
+  //   uiConsole(address);
+  // };
 
-//   const getBalance = async () => {
-//     if (!provider) {
-//       uiConsole("provider not initialized yet");
-//       return;
-//     }
-//     const balance = await RPC.getBalance(provider);
-//     uiConsole(balance);
-//   };
+  // const getBalance = async () => {
+  //   if (!provider) {
+  //     uiConsole("provider not initialized yet");
+  //     return;
+  //   }
+  //   const balance = await RPC.getBalance(provider);
+  //   uiConsole(balance);
+  // };
 
-//   const signMessage = async () => {
-//     if (!provider) {
-//       uiConsole("provider not initialized yet");
-//       return;
-//     }
-//     const signedMessage = await RPC.signMessage(provider);
-//     uiConsole(signedMessage);
-//   };
+  // const signMessage = async () => {
+  //   if (!provider) {
+  //     uiConsole("provider not initialized yet");
+  //     return;
+  //   }
+  //   const signedMessage = await RPC.signMessage(provider);
+  //   uiConsole(signedMessage);
+  // };
 
-//   const sendTransaction = async () => {
-//     if (!provider) {
-//       uiConsole("provider not initialized yet");
-//       return;
-//     }
-//     uiConsole("Sending Transaction...");
-//     const transactionReceipt = await RPC.sendTransaction(provider);
-//     uiConsole(transactionReceipt);
-//   };
+  // const sendTransaction = async () => {
+  //   if (!provider) {
+  //     uiConsole("provider not initialized yet");
+  //     return;
+  //   }
+  //   uiConsole("Sending Transaction...");
+  //   const transactionReceipt = await RPC.sendTransaction(provider);
+  //   uiConsole(transactionReceipt);
+  // };
 
-//   function uiConsole(...args: any[]): void {
-//     const el = document.querySelector("#console>p");
-//     if (el) {
-//       el.innerHTML = JSON.stringify(args || {}, null, 2);
-//       console.log(...args);
-//     }
-//   }
+  // function uiConsole(...args: any[]): void {
+  //   const el = document.querySelector("#console>p");
+  //   if (el) {
+  //     el.innerHTML = JSON.stringify(args || {}, null, 2);
+  //     console.log(...args);
+  //   }
+  // }
 
-//   const loggedInView = (
-//     <>
-//       <div className="flex-container">
-//         <div>
-//           <button onClick={getUserInfo} className="card">
-//             Get User Info
-//           </button>
-//         </div>
-//         <div>
-//           <button onClick={getAccounts} className="card">
-//             Get Accounts
-//           </button>
-//         </div>
-//         <div>
-//           <button onClick={getBalance} className="card">
-//             Get Balance
-//           </button>
-//         </div>
-//         <div>
-//           <button onClick={signMessage} className="card">
-//             Sign Message
-//           </button>
-//         </div>
-//         <div>
-//           <button onClick={sendTransaction} className="card">
-//             Send Transaction
-//           </button>
-//         </div>
-//         <div>
-//           <button onClick={logout} className="card">
-//             Log Out
-//           </button>
-//         </div>
-//       </div>
-//     </>
-//   );
+  // const loggedInView = (
+  //   <>
+  //     <div className="flex-container">
+  //       <div>
+  //         <button onClick={getUserInfo} className="card">
+  //           Get User Info
+  //         </button>
+  //       </div>
+  //       <div>
+  //         <button onClick={getAccounts} className="card">
+  //           Get Accounts
+  //         </button>
+  //       </div>
+  //       <div>
+  //         <button onClick={getBalance} className="card">
+  //           Get Balance
+  //         </button>
+  //       </div>
+  //       <div>
+  //         <button onClick={signMessage} className="card">
+  //           Sign Message
+  //         </button>
+  //       </div>
+  //       <div>
+  //         <button onClick={sendTransaction} className="card">
+  //           Send Transaction
+  //         </button>
+  //       </div>
+  //       <div>
+  //         <button onClick={logout} className="card">
+  //           Log Out
+  //         </button>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 
-//   const unloggedInView = (
-//     <button onClick={login} className="card">
-//       Login
-//     </button>
-//   );
+  // const unloggedInView = (
+  //   <button onClick={login} className="card">
+  //     Login
+  //   </button>
+  // );
 
-//   return (
-//     <div className="container">
-//       <h1 className="title">
-//         <a target="_blank" href="https://web3auth.io/docs/sdk/pnp/web/modal" rel="noreferrer">
-//           Web3Auth{" "}
-//         </a>
-//         & NextJS Quick Start
-//       </h1>
+  // return (
+  //   <div className="container">
+  //     <h1 className="title">
+  //       <a target="_blank" href="https://web3auth.io/docs/sdk/pnp/web/modal" rel="noreferrer">
+  //         Web3Auth{" "}
+  //       </a>
+  //       & NextJS Quick Start
+  //     </h1>
 
-//       <div className="grid">{loggedIn ? loggedInView : unloggedInView}</div>
-//       <div id="console" style={{ whiteSpace: "pre-line" }}>
-//         <p style={{ whiteSpace: "pre-line" }}></p>
-//       </div>
+  //     <div className="grid">{loggedIn ? loggedInView : unloggedInView}</div>
+  //     <div id="console" style={{ whiteSpace: "pre-line" }}>
+  //       <p style={{ whiteSpace: "pre-line" }}></p>
+  //     </div>
 
-//       <footer className="footer">
-//         <a
-//           href="https://github.com/Web3Auth/web3auth-pnp-examples/tree/main/web-modal-sdk/quick-starts/nextjs-modal-quick-start"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Source code
-//         </a>
-//         <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FWeb3Auth%2Fweb3auth-pnp-examples%2Ftree%2Fmain%2Fweb-modal-sdk%2Fquick-starts%2Fnextjs-modal-quick-start&project-name=w3a-nextjs-modal&repository-name=w3a-nextjs-modal">
-//           <img src="https://vercel.com/button" alt="Deploy with Vercel" />
-//         </a>
-//       </footer>
-//     </div>
-//   );
-// }
-
-// export default App;
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+  //     <footer className="footer">
+  //       <a
+  //         href="https://github.com/Web3Auth/web3auth-pnp-examples/tree/main/web-modal-sdk/quick-starts/nextjs-modal-quick-start"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Source code
+  //       </a>
+  //       <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FWeb3Auth%2Fweb3auth-pnp-examples%2Ftree%2Fmain%2Fweb-modal-sdk%2Fquick-starts%2Fnextjs-modal-quick-start&project-name=w3a-nextjs-modal&repository-name=w3a-nextjs-modal">
+  //         <img src="https://vercel.com/button" alt="Deploy with Vercel" />
+  //       </a>
+  //     </footer>
+  //   </div>
+  // );
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    <div></div>
+  )
 }
