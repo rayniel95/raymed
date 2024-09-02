1f6c8b34fce3afef9343adf696e881ba6a56c69c
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-09-01 20:50:03 -0400

    add rainbowkit config

--- frontend/ui/src/app/layout.tsx
+++ frontend/ui/src/app/layout.tsx
@@ -3,7 +3,29 @@
 
 "use client";
 
-// import "./globals.css";
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  sepolia
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+
+const config = getDefaultConfig({
+  appName: 'raymed',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [sepolia],
+  ssr: false, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
@@ -12,7 +34,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+            <RainbowKitProvider>
+              {children}
+            </RainbowKitProvider>
+          </QueryClientProvider>
+        </WagmiProvider>
+      </body>
     </html>
   );
 }
