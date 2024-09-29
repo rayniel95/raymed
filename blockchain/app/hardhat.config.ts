fca9ba72af88aee72ff0226c1d1d0809198c07b7
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-09-29 15:17:49 -0400

    change the chainif of the default network to be used by localhost metamask

--- blockchain/app/hardhat.config.ts
+++ blockchain/app/hardhat.config.ts
@@ -4,6 +4,11 @@ import "@nomicfoundation/hardhat-ignition-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    }
+  }
 };
 
 export default config;
