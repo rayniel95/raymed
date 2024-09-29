f10d90b131f760f99876e10c7a6b3c4e809640b4
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-09-29 15:20:06 -0400

    transform mapper into a record to improve type safety

--- frontend/ui/src/app/api/dataprovider/pathToContractMapper.ts
+++ frontend/ui/src/app/api/dataprovider/pathToContractMapper.ts
@@ -1,7 +1,9 @@
-export const mapper = {
-    "admin": process.env.NEXT_PUBLIC_ADMIN_CONTRACT_ADDRESS! as "0x{string}",
-    "doctor": process.env.NEXT_PUBLIC_DOCTOR_CONTRACT_ADDRESS! as "0x{string}",
-    "patient": process.env.NEXT_PUBLIC_PATIENT_CONTRACT_ADDRESS! as "0x{string}",
-    "drugExposure": process.env.NEXT_PUBLIC_DRUG_EXPOSURE_CONTRACT_ADDRESS! as "0x{string}",
-    "note": process.env.NEXT_PUBLIC_NOTE_CONTRACT_ADDRESS! as "0x{string}",
+import { Routes } from "../routes"
+
+export const mapper: Record<Routes, "0x{string}"> = {
+    "admins": process.env.NEXT_PUBLIC_ADMIN_CONTRACT_ADDRESS! as "0x{string}",
+    "doctors": process.env.NEXT_PUBLIC_DOCTOR_CONTRACT_ADDRESS! as "0x{string}",
+    "patients": process.env.NEXT_PUBLIC_PATIENT_CONTRACT_ADDRESS! as "0x{string}",
+    "drugExposures": process.env.NEXT_PUBLIC_DRUG_EXPOSURE_CONTRACT_ADDRESS! as "0x{string}",
+    "notes": process.env.NEXT_PUBLIC_NOTE_CONTRACT_ADDRESS! as "0x{string}",
 }
