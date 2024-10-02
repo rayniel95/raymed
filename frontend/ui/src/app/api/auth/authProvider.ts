a776fbeb372d29509095a3f67229dcc281edaf5e
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-10-01 21:06:33 -0400

    use a function to check params in the auth provider

--- frontend/ui/src/app/api/auth/authProvider.ts
+++ frontend/ui/src/app/api/auth/authProvider.ts
@@ -1,16 +1,33 @@
-const authProvider = {
-    // send username and password to the auth server and get back credentials
-    login: () => Promise.resolve(),
-    // when the dataProvider returns an error, check if this is an authentication error
-    checkError: () => Promise.resolve(),
-    // when the user navigates, make sure that their credentials are still valid
-    checkAuth: () => Promise.resolve(),
-    // remove local credentials and notify the auth server that the user logged out
-    logout: () => Promise.resolve(),
-    // get the user's profile
-    getIdentity: () => Promise.reject(),
-    // get the user permissions (optional)
-    getPermissions: () => Promise.resolve(),
-};
-//TODO - put auth inside api
-export default authProvider;
+import { AuthProvider, QueryFunctionContext } from 'react-admin'
+import { UseWalletClientReturnType } from 'wagmi';
+import { account } from '@/app/blockchain/account';
+
+export function authProvider(
+    walletClient: UseWalletClientReturnType,
+    isConnected:boolean
+): AuthProvider {
+    return {
+        // send username and password to the auth server and get back credentials
+        login: () => Promise.resolve(),
+        // when the dataProvider returns an error, check if this is an authentication error
+        checkError: () => Promise.resolve(),
+        // when the user navigates, make sure that their credentials are still valid
+        checkAuth: (params: any & QueryFunctionContext) => {
+            console.log(`windows: ${window.location}`)
+            if (
+                window.location.pathname.includes('create') ||
+                window.location.pathname.includes('edit') ||
+                window.location.pathname.includes('delete')
+            ) {
+                
+            }
+            return Promise.resolve()
+        },
+        // remove local credentials and notify the auth server that the user logged out
+        logout: () => Promise.resolve(),
+        // get the user's profile
+        getIdentity: () => Promise.reject(),
+        // get the user permissions (optional)
+        getPermissions: () => Promise.resolve(),
+    };
+}
