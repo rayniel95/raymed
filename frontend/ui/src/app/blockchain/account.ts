be6c653613240d8c87679c7acaaef9f8e777ee5a
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-10-16 17:52:45 -0400

    check if the wallet is connected

--- frontend/ui/src/app/blockchain/account.ts
+++ frontend/ui/src/app/blockchain/account.ts
@@ -1,4 +1,36 @@
-import { config } from "./config";
-import { getAccount } from "wagmi/actions";
+import { HttpError } from "react-admin";
 
-export const account = getAccount(config)
+function isWalletConnected(): boolean {
+    // Check if the ethereum object exists
+    if (typeof window.ethereum === 'undefined') {
+        console.log('No web3 provider detected');
+        return false;
+    }
+
+    // Attempt to request accounts
+    return window.ethereum.request({ method: 'eth_accounts' })
+        .then((accounts: any) => {
+            if (accounts.length > 0) {
+                console.log('Wallet is connected');
+                return true;
+            } else {
+                console.log('No accounts found');
+                return false;
+            }
+        })
+        .catch((error: any) => {
+            console.error('Error:', error);
+            return false;
+        });
+}
+
+export function checkWalletConnection(){
+    if(!isWalletConnected()){
+        console.log("some text here")
+        throw new HttpError(
+            "You need to connect the wallet to create a new record",
+            401,
+            "Unauthorized"
+        )
+    }
+}
