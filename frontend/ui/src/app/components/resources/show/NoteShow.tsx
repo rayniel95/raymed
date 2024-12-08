1dbb1a45e8303494056e23bbe880134d0248b144
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-12-08 15:10:49 -0500

    fix show component

--- frontend/ui/src/app/components/resources/show/NoteShow.tsx
+++ frontend/ui/src/app/components/resources/show/NoteShow.tsx
@@ -1,10 +1,10 @@
-import { List, Datagrid, TextField, NumberField, SelectField, Show, SimpleShowLayout } from "react-admin";
+import { TextField, NumberField, Show, SimpleShowLayout, ReferenceField } from "react-admin";
 //TODO - add relations between resources
 export const NoteShow = () => (
     <Show>
         <SimpleShowLayout>
             <NumberField source="id" />
-            <NumberField source="patientIdentifier" />
+            <TextField source="owner" />
             <TextField source="title" />
             <TextField source="date" />
             <TextField source="text" />
