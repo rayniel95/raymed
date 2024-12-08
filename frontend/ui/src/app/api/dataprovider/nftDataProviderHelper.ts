3184e0e431a32faa9413bf7de1605a35415459e3
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-12-08 15:07:12 -0500

    add base function to create data providers path based

--- frontend/ui/src/app/api/dataprovider/nftDataProviderHelper.ts
+++ frontend/ui/src/app/api/dataprovider/nftDataProviderHelper.ts
@@ -1,3 +1,8 @@
+import { UseClientReturnType, UseWalletClientReturnType } from "wagmi";
+import nftDataProvider from "./nftDataProvider";
+import { Helia } from "helia";
+import { combineDataProviders, DataProvider } from "react-admin";
+import nftHistoryDataProvider from "./nftDataProviderHistory";
 
 export function transformModelToDashboard<T>(
     model: T, id: number
@@ -8,3 +13,36 @@ export function transformModelToDashboard<T>(
         ...model
     }
 }
+
+export function createDataProvider(
+    mapper: Record<string, "0x{string}">,
+    publicClient: UseClientReturnType, //TODO - set the appropiate type from public client
+    walletClient: UseWalletClientReturnType,
+    heliaNode: Helia
+): DataProvider 
+{
+    const usualDataProvider = nftDataProvider(
+        mapper,
+        publicClient,
+        walletClient,
+        heliaNode!
+    )
+    const historyDataProvider = nftHistoryDataProvider(
+        mapper,
+        publicClient,
+        walletClient,
+        heliaNode!
+    )
+
+    return combineDataProviders(function(resource: string) {
+        switch (resource) {
+            case 'posts':
+            case 'comments':
+                return usualDataProvider;
+            case 'users':
+                return historyDataProvider;
+            default:
+                throw new Error(`Unknown resource: ${resource}`);
+        }
+    });
+}    
