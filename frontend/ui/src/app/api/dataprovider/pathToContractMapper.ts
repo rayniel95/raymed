c863fbd1889472565488597af3334402080d95b5
Rainyel Ramos <rainyel.ramos@gmail.com>
2025-01-02 16:22:52 -0500

    add a mapper from routes to graphql function

--- frontend/ui/src/app/api/dataprovider/pathToContractMapper.ts
+++ frontend/ui/src/app/api/dataprovider/pathToContractMapper.ts
@@ -1,4 +1,5 @@
-import { Routes } from "../routes"
+import { Routes, RoutesHistory } from "../routes"
+import {noteQuery, drugExposureQuery} from "@/app/api/queries/graphqlClient"
 
 export const mapper: Record<Routes, "0x{string}"> = {
     "admins": process.env.NEXT_PUBLIC_ADMIN_CONTRACT_ADDRESS! as "0x{string}",
@@ -6,4 +7,9 @@ export const mapper: Record<Routes, "0x{string}"> = {
     "patients": process.env.NEXT_PUBLIC_PATIENT_CONTRACT_ADDRESS! as "0x{string}",
     "drugExposures": process.env.NEXT_PUBLIC_DRUG_EXPOSURE_CONTRACT_ADDRESS! as "0x{string}",
     "notes": process.env.NEXT_PUBLIC_NOTE_CONTRACT_ADDRESS! as "0x{string}",
+}
+
+export const mapperHistory: Record<RoutesHistory, (arg0: number, arg1: number)=> Promise<any>> = {
+    "drugExposures/history": drugExposureQuery,
+    "notes/history": noteQuery,
 }
