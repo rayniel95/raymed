4db6051917338979a0a504d7e64b8490ef311982
Rainyel Ramos <rainyel.ramos@gmail.com>
2025-01-02 19:55:35 -0500

    fix error in execute function call

--- frontend/ui/src/app/api/queries/graphqlClient.ts
+++ frontend/ui/src/app/api/queries/graphqlClient.ts
@@ -1,15 +1,13 @@
 import {execute, DrugExposureDocument, DrugExposureQuery, NoteQuery, NoteDocument} from '@/app/api/queries/.graphclient'
 
 export async function drugExposureQuery(first: number, skip: number) {
-  return await execute<DrugExposureQuery>({
-    query: DrugExposureDocument,
-    variables: {first, skip},
+  return await execute<DrugExposureQuery>(DrugExposureDocument, {
+  first, skip
   })
 }
 
 export async function noteQuery(first: number, skip: number) {
-  return await execute<NoteQuery>({
-    query: NoteDocument,
-    variables: {first, skip},
+  return await execute<NoteQuery>(NoteDocument, {
+    first, skip
   })
 }
