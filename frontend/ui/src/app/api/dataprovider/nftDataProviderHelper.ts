5315b9416cb2886036980314fee554c7728c6b56
Rainyel Ramos <rainyel.ramos@gmail.com>
2025-01-03 20:19:16 -0500

    fix error due to id property being overwriten

--- frontend/ui/src/app/api/dataprovider/nftDataProviderHelper.ts
+++ frontend/ui/src/app/api/dataprovider/nftDataProviderHelper.ts
@@ -11,8 +11,8 @@ export function transformModelToDashboard<T>(
 ): T & { id: number }//TODO - how to add a t that extend rarecord
 {
     return {
-        id,
-        ...model
+        ...model,
+        id
     }
 }
 
