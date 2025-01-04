db4a2f4744f43b5f03864403a20831a9a426bcae
Rainyel Ramos <rainyel.ramos@gmail.com>
2025-01-03 20:15:24 -0500

    type the return values of functions

--- frontend/ui/src/app/api/queries/graphqlClient.ts
+++ frontend/ui/src/app/api/queries/graphqlClient.ts
@@ -1,12 +1,13 @@
 import {execute, DrugExposureDocument, DrugExposureQuery, NoteQuery, NoteDocument} from '@/app/api/queries/.graphclient'
+import { TDrugExposureQueryResult, TNoteQueryResult } from './types'
 
-export async function drugExposureQuery(first: number, skip: number) {
+export async function drugExposureQuery(first: number, skip: number): Promise<TDrugExposureQueryResult> {
   return await execute<DrugExposureQuery>(DrugExposureDocument, {
   first, skip
   })
 }
 
-export async function noteQuery(first: number, skip: number) {
+export async function noteQuery(first: number, skip: number): Promise<TNoteQueryResult> {
   return await execute<NoteQuery>(NoteDocument, {
     first, skip
   })
