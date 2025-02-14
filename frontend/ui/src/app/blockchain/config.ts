3c84b92030340b7b7bbde7d01c9fa885eeb66b79
Rainyel Ramos <rainyel.ramos@gmail.com>
2025-02-14 16:57:48 -0500

    fix error with rate in public node

--- frontend/ui/src/app/blockchain/config.ts
+++ frontend/ui/src/app/blockchain/config.ts
@@ -3,12 +3,16 @@ import {
 } from '@rainbow-me/rainbowkit';
 import {
     localhost,
-    sepolia
+    sepolia,
 } from 'wagmi/chains';
+import { http } from 'wagmi';
 
 export const config = getDefaultConfig({
     appName: 'raymed',
     projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
     chains: [sepolia, localhost],
     ssr: false, // If your dApp uses server side rendering (SSR)
+    transports:{
+        [sepolia.id]: http(`https://eth-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_APY_KEY}`),
+    }
 });
