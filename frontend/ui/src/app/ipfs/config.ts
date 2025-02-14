3bb04acdd59bb76ef1979007b56e0a5c7e662c4c
Rainyel Ramos <rainyel.ramos@gmail.com>
2025-02-14 16:58:15 -0500

    fix env var name error

--- frontend/ui/src/app/ipfs/config.ts
+++ frontend/ui/src/app/ipfs/config.ts
@@ -2,5 +2,5 @@ import { PinataSDK } from "pinata-web3"
 
 export const pinata = new PinataSDK({
   pinataJwt: `${process.env.NEXT_PUBLIC_PINATA_JWT}`,
-  pinataGateway: `${process.env.NEXT_PUBLIC_GATEWAY_URL}`
+  pinataGateway: `${process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL}`
 })
